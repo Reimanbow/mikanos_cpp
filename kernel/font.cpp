10eb8ec3a9cf068fd565b8406502339afd8aa070
Reimanbow <reima@cps.im.dendai.ac.jp>
2025-07-18 09:10:32 +0900

    day05d: newlibクラスの使用

--- kernel/font.cpp
+++ kernel/font.cpp
@@ -34,3 +34,10 @@ void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& col
 		}
 	}
 }
+
+// 文字列描画
+void WriteString(PixelWriter& writer, int x, int y, const char* s, const PixelColor& color) {
+	for (int i = 0; s[i] != '\0'; i++) {
+		WriteAscii(writer, x + 8 * i, y, s[i], color);
+	}
+}
--- kernel/font.hpp
+++ kernel/font.hpp
@@ -5,3 +5,5 @@
 
 // 作成したフォントデータを利用して1文字を描画するための関数
 void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color);
+// 文字列描画
+void WriteString(PixelWriter& writer, int x, int y, const char* s, const PixelColor& color);
